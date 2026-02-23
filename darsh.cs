6c7fcb5447c3cd506996de2a30b8981560518fe6
kolladarshan <161349885+kolladarshan@users.noreply.github.com>
2026-02-23 16:38:06 +0530

    Update darsh.cs

---
+++ darsh.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace firstthing
+{
+    class darsh
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables Practice");
+
+    int age = 22;
+    string name = "Darshan";
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        int number = 10;
+        if (number > 5)
+    {
+        Console.WriteLine("Number is greater than 5");
+    }
+    else
+    {
+        Console.WriteLine("Number is small");
+    }
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.WriteLine("Count: " + i);
+    }
+        }
+    }
+}
